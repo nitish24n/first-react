7ea5d70120ee6567eb7c1e8455feafba82d65171
nitish24n <nitish24m@gmail.com>
2018-07-04 06:51:26 +0530

    mapping projects array into console using arrow function

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProjectItem extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <li className="Projects">
+
+      </li>
+    );
+  }
+}
+
+export default ProjectItem;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class Projects extends Component {
   render() {
-    console.log(this.props);
+    let projectItems;
+    if(this.props.projects){
+      projectItems = this.props.projects.map(project => {
+        console.log(project);
+      });
+    }
+
+    //console.log(this.props);
     return (
       <div className="Projects">
         My Projects
