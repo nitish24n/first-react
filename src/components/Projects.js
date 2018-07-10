66b5b567bcc9e22ceeb3937d4048b7813f715d5f
nitish24n <nitish24m@gmail.com>
2018-07-10 09:59:46 +0530

    Revert "passed data to ProjectItem"
    This reverts commit d3e43a962b06fa861d367d7a9fa479178f3cbfa6.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
   render() {
     let projectItems;
     if(this.props.projects){
       projectItems = this.props.projects.map(project => {
-        //console.log(project);
-        return (
-          <ProjectItem key={project.title} project={project} />
-        );
+        console.log(project);
       });
     }
 
     //console.log(this.props);
     return (
       <div className="Projects">
-        {projectItems}
+        My Projects
       </div>
     );
   }
